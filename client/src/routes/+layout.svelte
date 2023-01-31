a3d7bd73ec0edf1efcfb83cd9632e6cb3a11d785
Soarnir <soarnir@gmail.com>
2023-01-31 14:58:28 +0100

    Test svg logo as brand
    Add base build action

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -3,10 +3,14 @@
   import 'bootstrap/js/dist/modal'
   import 'bootstrap/js/dist/button'
   import 'bootstrap/js/dist/tab'
+  import Logo from '$lib/assets/logo.svelte'
 </script>
 
 <nav class="navbar navbar-expand-lg bg-body">
   <div class="container-fluid">
+    <div class="navbar-brand">
+      <Logo sizeX="200" sizeY="100" animated={false} />
+    </div>
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
